37792c3177528ea78d6a9f2be8eeef3fc8868a31
arlindademaj <aarlindademaj@gmail.com>
2026-03-22 18:36:43 +0300

    feat: add CORS middleware to express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import { notFound } from "./middleware/notFound.middleware.js";
+import cors from "cors";
 import express from "express";
 import notesRouter from "./routes/notes.route.js";
 import { logger } from "./middleware/logger.middleware.js";
@@ -7,6 +8,7 @@ import { errorHandler } from "./middleware/error.middleware.js";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(logger);
 
 app.use("/notes", notesRouter);
